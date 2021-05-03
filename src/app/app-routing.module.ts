a200656006addae42d6500d78d01383592c619d5
prince <princeamitlali@gmail.com>
2021-05-03 10:48:17 +0530

    routing for contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'contact', component: ContactUsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
