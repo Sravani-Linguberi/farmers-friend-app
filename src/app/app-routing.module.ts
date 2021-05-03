16dcde3c96645b7f13169fff2e2670820656f36f
Richa Pandey <81300346+RichaPandey02@users.noreply.github.com>
2021-05-03 13:54:20 +0530

    register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [{ path: 'contact', component: ContactUsComponent }];
+const routes: Routes = [{ path: 'contact', component: ContactUsComponent },
+{path: 'register' ,component: RegisterComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
