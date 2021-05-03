c098875bbfbaf8b06edd6998685d7710653c1312
Richa Pandey <81300346+RichaPandey02@users.noreply.github.com>
2021-05-03 14:56:17 +0530

    login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(private readonly fb: FormBuilder) {
+    this.form = this.fb.group({
+      userName:  ['', Validators.required],
+      phoneNumber:  ['', Validators.required],
+    });
+   }
+   submitForm() {
+    if (this.form.valid) {
+      console.log(this.form.getRawValue());
+  } else {
+      console.log('There is a problem with the form');
+  }
+  }
 
   ngOnInit(): void {
   }
