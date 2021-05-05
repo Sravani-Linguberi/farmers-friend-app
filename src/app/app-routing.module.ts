0c7d4184d919163c84e9ec2bef965383fc2c73d9
Sravani-Linguberi <sravani.linguberi369@gmail.com>
2021-05-05 15:43:45 +0530

    routing module is edited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,15 +10,10 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 
 const routes: Routes = [{ path: 'contact', component: ContactUsComponent },
 {path: 'register' ,component: RegisterComponent},
-<<<<<<< HEAD
   { path: 'login', component: LoginComponent },
-  { path: 'home-page', component: HomePageComponent }
+  { path: 'home-page', component: HomePageComponent },
+  {path: 'price' , component:  MarketPriceComponent}
 ];
-
-=======
-{path: 'login',component:LoginComponent},
-{path: 'price' , component:  MarketPriceComponent}];
->>>>>>> 14256acd75ce73232928891686b9d720983e7a30
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
