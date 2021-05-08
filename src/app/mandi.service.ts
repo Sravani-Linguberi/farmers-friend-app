d2f7ba072bc67cd7eadc6df6d289089aa9e05789
Ankit <informtoankit@gmail.com>
2021-05-08 18:26:16 +0530

    Almost backend completed

--- src/app/mandi.service.ts
+++ src/app/mandi.service.ts
@@ -1,10 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient ,HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MandiService {
+  push(Mandi: any) {
+    throw new Error('Method not implemented.');
+  }
 
   constructor(private httpclient : HttpClient) { }
 
@@ -15,4 +18,9 @@ export class MandiService {
 
   }
 
-}
+  addMandi(newmandi:any)
+{
+  var headers= new HttpHeaders();
+  headers.append('mandi-type' , 'application/json ');
+  return this.httpclient.post<any>('http://localhost:3000/api/mandiSchema', newmandi,{headers:headers})
+}}
--- src/app/posts/post-mandi/post-mandi.component.ts
+++ src/app/posts/post-mandi/post-mandi.component.ts
@@ -1,3 +1,4 @@
+import { MandiService } from './../../mandi.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostMandiComponent implements OnInit {
 
-  constructor() { }
+
+  Mandi: any ;
+  STATE: any
+  DIST: any
+  MARKET: any
+  GODOWN:any
+  COLDSTORAGE: any
+  DISTANCE: any
+  CROPS:any
+  PHONE: any
+  ADDRESS:any
+
+  constructor(private mandiService : MandiService) { }
 
   ngOnInit(): void {
   }
+  addhelplines(){
 
+    const addMandi = {
+
+
+      STATE: this.STATE ,
+      DIST:this.DIST ,
+      MARKET: this.MARKET ,
+      GODOWN:this.GODOWN ,
+      COLDSTORAGE: this.COLDSTORAGE,
+      DISTANCE: this.DISTANCE ,
+      CROPS:this.CROPS ,
+      PHONE: this.PHONE,
+      ADDRESS:this.ADDRESS,
+
+
+
+
+       }
+       this.mandiService.addMandi(addMandi)
+       .subscribe(Mandi =>{
+         this.mandiService.push(Mandi);
+         this.mandiService.getmandiSchema()
+         .subscribe(helplines =>
+           this.Mandi=Mandi)
+       })
+
+     alert("Your details added Successfully")
+     }
 }
+
